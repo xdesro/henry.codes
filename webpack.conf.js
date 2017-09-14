33ad780d4ea01e66296845ae08963b914daab76e
Henry Desroches <yo@henry.codes>
2017-09-14 12:33:11 -0600

    UglifyJS for pageload

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,5 +1,6 @@
 import webpack from "webpack";
 import path from "path";
+import UglifyJSPlugin from "uglifyjs-webpack-plugin";
 
 export default {
   module: {
@@ -21,7 +22,8 @@ export default {
   plugins: [
     new webpack.ProvidePlugin({
       "fetch": "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
-    })
+    }),
+    new UglifyJSPlugin()
   ],
 
   context: path.join(__dirname, "src"),
